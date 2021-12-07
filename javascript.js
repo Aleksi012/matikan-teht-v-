83298934fcc3ebfc1957fa5a36d96db5cac9d131
Aleksi012 <aleksi.anttila001@gmail.com>
2021-12-07 15:05:46 +0200

    first commit

---
+++ javascript.js
@@ -0,0 +1,4 @@
+let randomNumber = Math.floor(Math.random(1) * 10);
+let laskut = document.getElementsByClassName("laskut")
+console.log(laskut)
+document.getElementsByClassName("laskut").innerText = "1"
