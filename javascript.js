f89eaf2acb129d8235c127ca9ff8b28705002adb
Aleksi012 <aleksi.anttila001@gmail.com>
2021-12-12 20:46:20 +0200

    commit

--- javascript.js
+++ javascript.js
@@ -3,13 +3,19 @@ let randomNumber2 = Math.floor(Math.random() * 10) + 1;
 let randomNumber3 = Math.floor(Math.random() * 10) + 1;
 let randomNumber4 = Math.floor(Math.random() * 10) + 1;
 let randomNumber5 = Math.floor(Math.random() * 10) + 1;
+let randomNumber6 = Math.floor(Math.random() * 10) + 1;
+let randomNumber7 = Math.floor(Math.random() * 10) + 1;
+let randomNumber8 = Math.floor(Math.random() * 10) + 1;
+let randomNumber9 = Math.floor(Math.random() * 10) + 1;
+let randomNumber10 = Math.floor(Math.random() * 10) + 1;
 
 
-document.getElementById("span").innerHTML = randomNumber1 + " x " + randomNumber5
-document.getElementById("span1").innerHTML = randomNumber2 + " x " + randomNumber4
-document.getElementById("span2").innerHTML = randomNumber3 + " x " + randomNumber3
-document.getElementById("span3").innerHTML = randomNumber4 + " x " + randomNumber2
-document.getElementById("span4").innerHTML = randomNumber5 + " x " + randomNumber1
+
+document.getElementById("span").innerHTML = randomNumber1 + " x " + randomNumber2 + " ="
+document.getElementById("span1").innerHTML = randomNumber3 + " x " + randomNumber4 + " ="
+document.getElementById("span2").innerHTML = randomNumber5 + " x " + randomNumber6 + " ="
+document.getElementById("span3").innerHTML = randomNumber7 + " x " + randomNumber8 + " ="
+document.getElementById("span4").innerHTML = randomNumber9 + " x " + randomNumber10 + " ="
 
 let vastaus = document.getElementById("button")
 
@@ -19,41 +25,42 @@ function myFunction() {
     input3 = document.getElementById("input3").value
     input4 = document.getElementById("input4").value
     input5 = document.getElementById("input5").value
-
-    console.log(input1)
-    console.log(input2)
-    console.log(input3)
-    console.log(input4)
-    console.log(input5)
     
-    if ( randomNumber1 * randomNumber5 == input1 ) {
+    if ( randomNumber1 * randomNumber2 == input1 ) {
         document.getElementById("vastaus1").innerHTML = "Oikein"
     } else {
         document.getElementById("vastaus1").innerHTML = "Väärin"
+        document.getElementById("oikeavastaus1").innerHTML = randomNumber1 * randomNumber2
     }
 
-    if ( randomNumber2 * randomNumber4 == input2 ) {
+    if ( randomNumber3 * randomNumber4 == input2 ) {
         document.getElementById("vastaus2").innerHTML = "Oikein"
     } else {
         document.getElementById("vastaus2").innerHTML = "Väärin"
+        document.getElementById("oikeavastaus2").innerHTML = randomNumber3 * randomNumber4
     }
 
-    if ( randomNumber3 * randomNumber3 == input3 ) {
+    if ( randomNumber5 * randomNumber6 == input3 ) {
         document.getElementById("vastaus3").innerHTML = "Oikein"
     } else {
         document.getElementById("vastaus3").innerHTML = "Väärin"
+        document.getElementById("oikeavastaus3").innerHTML = randomNumber5 * randomNumber6
     }
 
-    if ( randomNumber4 * randomNumber2 == input4 ) {
+    if ( randomNumber7 * randomNumber8 == input4 ) {
         document.getElementById("vastaus4").innerHTML = "Oikein"
     } else {
         document.getElementById("vastaus4").innerHTML = "Väärin"
+        document.getElementById("oikeavastaus4").innerHTML = randomNumber7 * randomNumber8
     }
 
-    if ( randomNumber5 * randomNumber1 == input5 ) {
+    if ( randomNumber9 * randomNumber10 == input5 ) {
         document.getElementById("vastaus5").innerHTML = "Oikein"
     } else {
         document.getElementById("vastaus5").innerHTML = "Väärin"
+        document.getElementById("oikeavastaus5").innerHTML = randomNumber9 * randomNumber10
     }
+
+    myFunction = null
 } 
 
